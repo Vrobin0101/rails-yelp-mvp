69070841837600d930d36ffced1d829fc209252c
Robin Varetz <vrobin@student.42.fr>
2022-05-18 14:35:17 +0200

    finish exo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: %i[show new create]
+  end
+  resources :reviews, only: %i[edit update destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+10.times do
+  restaurant = Restaurant.new(name: Faker::Restaurant.name, address:
+    Faker::Address.full_address, phone_number: Faker::Number.leading_zero_number(digits: 10), category: category.sample)
+    review = Review.new(content: Faker::Restaurant.review, rating: rand(0..5))
+  restaurant.save!
+  review.restaurant = restaurant
+  review.save!
+end
